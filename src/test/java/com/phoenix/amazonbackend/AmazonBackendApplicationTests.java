66eb25d77f878d039edd881b9922fd8560da3e32
BKD471 <bhaskarkumardas9@gmail.com>
2024-05-31 16:28:22 +0530

    Setting Up The Code Base

---
+++ src/test/java/com/phoenix/amazonbackend/AmazonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.amazonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
