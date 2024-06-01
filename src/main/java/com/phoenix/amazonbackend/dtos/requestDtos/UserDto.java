fd205d732dd3d422cdbc7ee31d7565514ff85eeb
BKD471 <bhaskarkumardas9@gmail.com>
2024-06-01 21:31:30 +0530

    Fixed the Dtos

--- src/main/java/com/phoenix/amazonbackend/dtos/requestDtos/UserDto.java
+++ src/main/java/com/phoenix/amazonbackend/dtos/requestDtos/UserDto.java
@@ -6,7 +6,7 @@ import lombok.Builder;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static com.phoenix.amazonbackend.utils.GenderMapHelpers.getGender;
+import static com.phoenix.amazonbackend.utils.GenderMapingHelpers.getGender;
 
 @Builder
 public record UserDto(UUID userId,
@@ -24,7 +24,7 @@ public record UserDto(UUID userId,
                       LocalDateTime modifiedDate,
                       String modifiedBy) {
 
-    public UserDto mapToUsersDto(final Users users) {
+    public static UserDto mapToUsersDto(final Users users) {
         return UserDto.builder()
                 .userId(users.getUserId())
                 .userName(users.getUserName())
@@ -54,35 +54,6 @@ public record UserDto(UUID userId,
                 .gender(getGender(userDto.gender()))
                 .profileImage(userDto.profileImage())
                 .about(userDto.about())
-                .build();
-    }
-
-    public Users mapToUsers(final UpdateUserDetailsDto updateUserDetailsDto) {
-        return Users.builder()
-                .userId(UUID.fromString(updateUserDetailsDto.userId()))
-                .userName(updateUserDetailsDto.userName())
-                .firstName(updateUserDetailsDto.firstName())
-                .lastName(updateUserDetailsDto.lastName())
-                .primaryEmail(updateUserDetailsDto.primaryEmail())
-                .secondaryEmail(updateUserDetailsDto.secondaryEmail())
-                .gender(getGender(updateUserDetailsDto.gender()))
-                .about(updateUserDetailsDto.about())
-                .build();
-    }
-
-    public UpdateUserDetailsDto mapToUserDto(final Users users) {
-        return UpdateUserDetailsDto.builder()
-                .userId(String.valueOf(users.getUserId()))
-                .userName(users.getUserName())
-                .firstName(users.getFirstName())
-                .lastName(users.getLastName())
-                .primaryEmail(users.getPrimaryEmail())
-                .secondaryEmail(users.getSecondaryEmail())
-                .gender(users.getGender().toString())
-                .about(users.getAbout())
-                .createdDate(users.getCreatedDate())
-                .modifiedBy(users.getModifiedBy())
-                .modifiedDate(users.getModifiedDate())
                 .build();
     }
 }
