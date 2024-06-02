f9187b8deca15291001ec734b918f0853689b673
BKD471 <bhaskarkumardas9@gmail.com>
2024-06-02 16:09:46 +0530

    Added User Controller

--- src/main/java/com/phoenix/amazonbackend/controllers/UserControllerImpl.java
+++ src/main/java/com/phoenix/amazonbackend/controllers/UserControllerImpl.java
@@ -7,50 +7,68 @@ import com.phoenix.amazonbackend.dtos.requestDtos.UserDto;
 import com.phoenix.amazonbackend.dtos.responseDtos.ApiResponse;
 import com.phoenix.amazonbackend.dtos.responseDtos.PageableResponse;
 import com.phoenix.amazonbackend.dtos.responseDtos.PasswordResponseMessages;
-import com.phoenix.amazonbackend.utils.AllConstants;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.phoenix.amazonbackend.utils.AllConstants.USER_FIELDS;
 
 
 @RestController("UserControllerPrimary")
 @AllArgsConstructor
 public class UserControllerImpl implements IUserController {
 
-
     @Override
-    public ResponseEntity<UserDto> createUser(UserDto user) {
+    public ResponseEntity<UserDto> createUser(final UserDto user) {
         return null;
     }
 
     @Override
-    public ResponseEntity<UserDto> updateUserByUserIdOrUserNameOrPrimaryEmail(UpdateUserDetailsDto user, String userId, String userName, String primaryEmail) {
+    public ResponseEntity<UserDto> updateUserByUserIdOrUserNameOrPrimaryEmail(final UpdateUserDetailsDto user,
+                                                                              final String userId,
+                                                                              final String userName,
+                                                                              final String primaryEmail) {
         return null;
     }
 
     @Override
-    public ResponseEntity<ApiResponse> deleteUserByUserIdOrUserNameOrPrimaryEmail(String userId, String userName, String primaryEmail) {
+    public ResponseEntity<ApiResponse> deleteUserByUserIdOrUserNameOrPrimaryEmail(final String userId,
+                                                                                  final String userName,
+                                                                                  final String primaryEmail) {
         return null;
     }
 
     @Override
-    public ResponseEntity<PageableResponse<UserDto>> getAllUsers(int pageNumber, int pageSize, String sortBy, String sortDir) {
+    public ResponseEntity<PageableResponse<UserDto>> getAllUsers(final int pageNumber,
+                                                                 final int pageSize,
+                                                                 final String sortBy,
+                                                                 final String sortDir) {
         return null;
     }
 
     @Override
-    public ResponseEntity<UserDto> getUserInformationByUserIdOrUserNameOrPrimaryEmail(String userId, String userName, String primaryEmail) {
+    public ResponseEntity<UserDto> getUserInformationByUserIdOrUserNameOrPrimaryEmail(final String userId,
+                                                                                      final String userName,
+                                                                                      final String primaryEmail) {
         return null;
     }
 
     @Override
-    public ResponseEntity<PageableResponse<UserDto>> searchUserByFieldAndValue(AllConstants.USER_FIELDS field, String value, int pageNumber, int pageSize, AllConstants.USER_FIELDS sortBy, String sortDir) {
+    public ResponseEntity<PageableResponse<UserDto>> searchUserByFieldAndValue(final USER_FIELDS field,
+                                                                               final String value,
+                                                                               final int pageNumber,
+                                                                               final int pageSize,
+                                                                               final USER_FIELDS sortBy,
+                                                                               final String sortDir) {
         return null;
     }
 
     @Override
-    public ResponseEntity<PageableResponse<UserDto>> searchAllUsersByUserName(String userNameWord, int pageNumber, int pageSize, String sortBy, String sortDir) {
+    public ResponseEntity<PageableResponse<UserDto>> searchAllUsersByUserName(final String userNameWord,
+                                                                              final int pageNumber,
+                                                                              final int pageSize,
+                                                                              final String sortBy,
+                                                                              final String sortDir) {
         return null;
     }
 
@@ -60,7 +78,7 @@ public class UserControllerImpl implements IUserController {
     }
 
     @Override
-    public ResponseEntity<PasswordResponseMessages> resetMyPassword(PasswordUpdateDto passwordUpdateDto) {
+    public ResponseEntity<PasswordResponseMessages> resetMyPassword(final PasswordUpdateDto passwordUpdateDto) {
         return null;
     }
 }
